3f4cbd848049df23a266e71bfc08dfb3165715fa
fe-oliver <nandacelia.dias@gmail.com>
2024-06-12 15:50:10 -0300

    commit dos operadores

--- estudos/metodos/metodos/src/App.java
+++ estudos/metodos/metodos/src/App.java
@@ -4,23 +4,23 @@
 // qual a visibilidade? será visivel a toda aplicação, somente em pacotes atraves de herança ou somente a nivel da proprio classe?
 
 // dois métodos, 
-public class MyClass {
+//public class MyClass {
     // aq temos um metodo q retorna o tipo Double e ele está publicom, visivel a todo sistema
-    public double somar (int num1, int num2){ // que ele tem finalidade de somar e tem 2 parametros inteiros 1 e 2)
+    //public double somar (int num1, int num2){ // que ele tem finalidade de somar e tem 2 parametros inteiros 1 e 2)
         // lógica - finalidade do método
-        return...; // se lá encima ele não é Void, então ele precisa retornar no final
-    }
-}
+        //return...; // se lá encima ele não é Void, então ele precisa retornar no final
+    //}
+//}
 
 // é publico, não retorna nada e espera um texto qualquer do tipo String
-public void imprimir(String texto){
+//public void imprimir(String texto){
     // logica, finalidade
     // não há return.
-}
+//}
 
 // aqui ele corre risco de disparar uma exceção
-public double dividir(int dividendo, int divisor) throws Exception{}
+//public double dividir(int dividendo, int divisor) throws Exception{}
 
 // nao pode ser visto por outras classes no programa
-private void metodoPrivado (){}
+//private void metodoPrivado (){}
 
