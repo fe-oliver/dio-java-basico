9cecbe3dba02dc6954cfb7ae24222184acc7cf0c
fe-oliver <nandacelia.dias@gmail.com>
2024-06-14 12:13:12 -0300

    att repetições 14/06

---
+++ estudos/estruturas-de-repeticao/exemplofor/src/ExemploFor.java
@@ -0,0 +1,25 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        /*for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando carneirinhos: " + carneirinhos);        
+    }*/
+
+
+    /* em Arrays o ínmdice inicia-se em 0. Uma coleção de elementos separados por vírgula.
+    String alunos [] = {"FELIPE", "FELIPE", "JULIA", "MARCOS"};
+    for (int x=0; x<alunos.length; x++) {
+        System.out.println("O aluno no indice x="+ x + " é " + alunos[x]);
+    } */
+
+
+    /* trabalhando com os elementos da array
+     *     String alunos [] = {"FELIPE", "FELIPE", "JULIA", "MARCOS"};
+    for(String aluno : alunos){
+        System.out.println("Nome do aluno é: "+ aluno);
+    }  
+    */
+     
+    /* 
+     */
+    }
+}
