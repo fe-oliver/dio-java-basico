85d6c6fbd89bdebb14f9e81573fff8323ef1d431
fe-oliver <nandacelia.dias@gmail.com>
2024-06-17 16:24:04 -0300

    aula 02

--- estudos/validacao-processo-seletivo/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ estudos/validacao-processo-seletivo/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,35 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
+
+    static void selecaoCandidatos() {
+        // array com candidatos
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase=2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato "+ candidato+" Solicitou este valor de salario: " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato "+ candidato+ " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido (){
+        return ThreadLocalRandom.current().nextDouble(1800, 2000);
+    }
+
     static void analisarCandidato (double salarioPretendido){
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido){
