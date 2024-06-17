d23638a58947a2c060d3c5acf94f4af89560db00
fe-oliver <nandacelia.dias@gmail.com>
2024-06-17 16:31:11 -0300

    aula 03

--- estudos/validacao-processo-seletivo/controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ estudos/validacao-processo-seletivo/controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -4,7 +4,19 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados () {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("IMprimindo a lista de candidatos informando o incide do elemento");
+        for (int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de numero "+ (indice+1) +" é "+ candidatos[indice]);
+        }
+        //forma abreviada de iteracao for each
+        for(String candidato: candidatos){
+            System.out.println("O candidato selecionado foi "+ candidato);
+        }
     }
 
     static void selecaoCandidatos() {
